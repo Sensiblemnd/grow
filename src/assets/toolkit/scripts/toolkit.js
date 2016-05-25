f13421d4fc4fbb0619fbf7eae671078fbba4cdd0
Rob Lloveras <sensiblemnd@gmil.com>
2016-05-25 19:50:57 -0400

    first commit

---
+++ src/assets/toolkit/scripts/toolkit.js
@@ -0,0 +1,75 @@
+/**
+ * Toolkit JavaScript
+ */
+// jshint unused:false
+// jshint esnext: true
+
+'use strict';
+
+
+const $ = require('jquery');
+
+//<button data-button='Add '></button>
+let config = {
+	debug: true,
+	istouch:false,
+	touchOrClick: '',
+	init: function(){
+		var $htmlElem=$('html');
+		this.istouch = $htmlElem.hasClass('touch')? true : false;
+		this.touchOrClick = this.istouch? 'touchend' :'click' ;
+	}
+
+};
+config.init();
+let  log = {
+	debug: function(content){
+		if ((window.console && window.console.log) && config.debug){
+			console.log(content);
+		}
+	}
+};
+
+let app = {
+	init: function() {
+		let that = this;
+		log.debug('asd');
+		that.eventListener();
+		that.mobileMenu();
+		
+	},
+	eventListener: function(){
+		let that = this;
+		log.debug('asd');
+
+		$('.instagram-image').on(config.touchOrClick, function(e) {
+			if($(this).hasClass('active')){
+				$(this).removeClass('active');
+			}else{
+				$('.instagram-image').removeClass('active');
+				$(this).toggleClass('active');
+			};	
+		});
+		
+	},
+	add: function(){
+		let that = this;
+	},
+	remove: function(){
+		let that = this;
+	},
+	mobileMenu: function() {
+		let that = this;
+
+		$('.hamburger-link').on(config.touchOrClick, function(e) {
+			e.preventDefault();
+			$('.hamburger').toggleClass('menu-open');
+			$('.side-menu').toggleClass('side-menu-open');
+		});
+	},
+	flipCard: function(e) {
+		
+	}
+};
+
+app.init();
---
+++ src/docs/javascript.md
@@ -0,0 +1,5 @@
+Work in progress.
+
+```javascript
+var foo = 'bar'
+```
